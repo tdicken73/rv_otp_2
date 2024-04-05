3641194b64e7cee6b403594f9f1287ce146fb790
Ben Elliott <belliott@redventures.com>
2024-04-05 09:51:52 -0600

    jumbotron module

---
+++ jumbotron/lib/jumbotron.ex
@@ -0,0 +1,18 @@
+defmodule Jumbotron do
+  @moduledoc """
+  Documentation for `Jumbotron`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Jumbotron.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
