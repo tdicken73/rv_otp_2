edef36f2fb698bbaa15a07d22088d57afb6dc9d0
Todd Dicken <tdicken@redventures.com>
2024-04-10 12:01:20 -0500

    evolve board

--- spring/lib/spring/grid.ex
+++ spring/lib/spring/grid.ex
@@ -1,17 +1,17 @@
 defmodule Spring.Grid do
   def new() do
-    w = 20
-    h = 20
-    grid = %{}
-
-    for x <- 1..w do
-      for y <- 1..h do
-        grid = Map.put(grid, {x, y}, Enum.random([true, false]))
-      end
-    end
+    w = 6
+    h = 6
+    for r <- 1..w, c <- 1..h, into: %{}, do: {{r, c}, Enum.random([true, false])}
   end
 
   def evolve(grid) do
-    grid
+
+    Enum.reduce(grid, %{}, fn {{rr, cc}, state}, acc ->
+      n_count =  for r <- (rr - 1)..(rr + 1), c <- (cc - 1)..(cc + 1), {r, c} != {rr, cc}, do: Map.get(grid, {r, c}, false)
+      new_state = Spring.Cell.evolve(state, n_count)
+      Map.put(acc, {rr, cc}, new_state)
+    end)
   end
+
 end
